dede771a400a64f272205f58ec8dbcb21a12f29d
deeksha567 <65412190+deeksha567@users.noreply.github.com>
2020-05-16 19:09:51 +0530

    Create Day3pythonprogram5.py

---
+++ Day3pythonprogram5.py
@@ -0,0 +1,10 @@
+list1 = [1, 2, 3,4,5]
+list2 = [1, 3, 5]
+
+intersection_set = set.intersection(set(list1), set(list2))
+
+
+intersection_list = list(intersection_set)
+
+print(intersection_list)
+
